abb9a646c6c57cb1df6fb6c1bae9a983045495dc
TheLucyClub Github Team <thelucyclub@gmail.com>
2016-06-29 12:11:36 -0400

    Update api

--- SafeConnect.php
+++ SafeConnect.php
@@ -18,7 +18,7 @@ public function onPlayerPreLoginEvent(PlayerPreLoginEvent $event) {
   $player = $event->getPlayer();
   Server::getInstance()->getLogger()->waring("$player is connecting... Please be patient");
   $player->sendTip(TF::BLUE . "[SafeConnect] $player does not pose a possible threat\n\n\n" . TF::LIGHT_PURPLE . "Your ping is below 500ms. Impressive!");
-  $output = shell_exec('ping -c1 api.thelucyclub2.yohosts.info/SafeConnect/List/raw');
+  $output = shell_exec('ping -c1 dl.thelucyclub.ml/ip_mcpe-deny.json');
   if($output == "" or $output == "down") {
   $player->close(TF::RED . "[SafeConnect] Can't reach API server at the moment. If you are the server owner, set the 'API-KEY' option in config.yml");
  }
